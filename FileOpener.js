1362da24db88bcd6ef8ca24119c3352d0893db19
hardikthakkar3 <hardikthakkar3@gmail.com>
2014-01-22 15:47:31 +0530

    Update FileOpener.js

--- FileOpener.js
+++ FileOpener.js
@@ -1,14 +1,14 @@
   
-  cordova.define("org.apache.cordova.fileopener.ChildBrowserCommand", function(require, exports, module) {
-                 var exec = require('cordova/exec');
-                 function ChildBrowser() {
-                 
-                 }
-                 ChildBrowser.prototype.open = function(url,name) {
-                      exec(null, null, "ChildBrowserCommand", "openPage", [url,name]);
-                 }
-                 
-                 var fileOpener = new ChildBrowser();
-                 module.exports = fileOpener;
-  
-  });
+cordova.define("org.apache.cordova.fileopener.ChildBrowserCommand", function(require, exports, module) {
+               var exec = require('cordova/exec');
+               function ChildBrowser() {
+               
+               }
+               ChildBrowser.prototype.open = function(url,name) {
+                    exec(null, null, "ChildBrowserCommand", "openPage", [url,name]);
+               }
+               
+               var fileOpener = new ChildBrowser();
+               module.exports = fileOpener;
+
+});
